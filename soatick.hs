988b9001704ea004e21297782a5e0c4c5584fadc
Ben Clifford <benc@hawaga.org.uk>
2020-12-31 13:20:27 +0000

    Initial git import discarding _darcs history, as used on my primary server for almost a decade

---
+++ soatick.hs
@@ -0,0 +1,56 @@
+
+-- soatick - generate a serial number suitable for use in a DNS SOA
+-- record, based on the current date, but keeping a state file so that
+-- if run multiple times on the same day, then a new serial number will
+-- be generated
+
+-- serial number format:
+
+-- 2011010502
+-- YYYYMMDDNN
+
+-- NN starts at 00 and increments by one each time soatick is called on the
+-- same day. If NN overflows, carrys will propagate treating the serial
+-- number as an integer (which may cause the DD and MM fields to move outside
+-- of the range expected for months and days).
+-- Calls to soatick on subsequent days will output values so that the serial
+-- number still increases - i.e. behaviour will still be safe.
+
+-- Algorithm:
+--   A = state file, incremented by 1. If the state file does not exist,
+--       treat it as containing 0.
+--   B = today's base serial number, NN=00
+--   Output = max(A,B)
+--   Output goes to state file and to stdout
+
+import Data.Time.Clock
+import Data.Time.Calendar
+import System.Directory
+import System.IO
+
+statefn = "soatick.state"
+
+main = do
+  hPutStrLn stderr "soatick (c) 2011-2020 Ben Clifford <benc@hawaga.org.uk>"
+  a <- serialFromState
+  b <- todaysBaseSerial
+  let o = a `max` b
+  print o
+  writeState o
+
+serialFromState = do
+  exists <- doesFileExist statefn
+  if exists then do
+    c <- readFile statefn
+    let d = (length c) `seq` (read c :: Integer)
+    return (d+1)
+   else return 0
+
+writeState s = writeFile statefn (show s)
+
+todaysBaseSerial = do
+   now <- (getCurrentTime :: IO UTCTime)
+   let (y,mi,di) = toGregorian (utctDay now)
+   let (m,d) = (toInteger mi, toInteger di)
+   return $ (((y*100 + m) * 100) + d) * 100
+
